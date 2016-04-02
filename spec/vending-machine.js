4afc562836266659b6bb8574fa01618f156bb0f0
Drew Keller <drew@eightycolumns.com>
2016-04-02 15:25:48 -0400

    Refactor spec/vending-machine.js

--- spec/vending-machine.js
+++ spec/vending-machine.js
@@ -1,11 +1,12 @@
 describe('Vending machine', function () {
   describe('A penny', function () {
+    var penny = Penny.create();
+
     it('weighs 2.5 g', function () {
-      var penny = Penny.create();
       expect(penny.getWeightInGrams()).toBe(2.5);
     });
+
     it('is 19.05 mm in diameter', function () {
-      var penny = Penny.create();
       expect(penny.getDiameterInMillimeters()).toBe(19.05);
     });
   });
